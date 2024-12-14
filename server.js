9aae1b6c2ad5dc64c5c0e6e65ce2e4ba825c8875
Kanaan Ziwar <k.ziwar@icloud.com>
2024-12-14 00:06:20 -0500

    DEMO COMMIT v2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
- res.send('Hello, Dockerized Node.js App! TESTING FOR DEMO AGAIN');
+ res.send('Hello, Dockerized Node.js App! TESTING FOR DEMO SO I CAN SLEEP');
 });
 app.listen(PORT, () => {
  console.log(`App running on http://localhost:${PORT}`);
