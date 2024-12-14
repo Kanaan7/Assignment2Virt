bfed2ceab75108b51d703ba561b7512e5c0a0304
Kanaan Ziwar <k.ziwar@icloud.com>
2024-12-14 00:10:36 -0500

    DEMO COMMIT trying to catch it up

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
- res.send('Hello, Dockerized Node.js App! TESTING FOR DEMO SO I CAN SLEEP');
+ res.send('Hello, Dockerized Node.js App! TESTING CAN THIS PLEASE CATCH UP');
 });
 app.listen(PORT, () => {
  console.log(`App running on http://localhost:${PORT}`);
